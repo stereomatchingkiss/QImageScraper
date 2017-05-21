c38c01281d1161cabb23ab95e37785b2a189ec47
stereomatchingkiss <thamngapwei@gmail.com>
2017-05-22 07:54:36 +0800

    add new function [js_scroll_to_window_height_2]

--- core/js_function.cpp
+++ core/js_function.cpp
@@ -16,18 +16,34 @@ QString doc_height()
 
 }
 
-QString js_scroll_to_window_height(qreal)
+QString js_scroll_to_window_height(qreal threshold)
 {
     return doc_height() + QString("\n"
                 "var dheight = doc_height();"
                 "function scrollPage(){"
                 "  var cur_height = window.innerHeight + window.pageYOffset;"
-                "  if(Math.abs(window.pageYOffset - document.body.scrollHeight) < 1000){"
+                "  if(Math.abs(window.pageYOffset - dheight) < %1){"
                 "    return [false, cur_height, dheight];"
                 "  }else{"
-                "    window.scrollTo(0, window.pageYOffset + 1000);"
+                "    window.scrollTo(0, window.pageYOffset + %1);"
                 "    return [true, cur_height, dheight];"
                 "  }"
                 "}"
-                "scrollPage()");
+                "scrollPage()").arg(threshold);
+}
+
+QString js_scroll_to_window_height_2(qreal threshold)
+{
+    return doc_height() + QString("\n"
+                "var dheight = doc_height();"
+                "function scrollPage(){"
+                "  var cur_height = window.innerHeight + window.pageYOffset;"
+                "  if(dheight < cur_height || Math.abs(dheight - window.pageYOffset) < %1){"
+                "    return [false, cur_height, dheight];"
+                "  }else{"
+                "    window.scrollTo(0, window.pageYOffset + Math.abs((dheight - window.pageYOffset)/2.0));"
+                "    return [true, cur_height, dheight];"
+                "  }"
+                "}"
+                "scrollPage()").arg(threshold);
 }
--- core/js_function.hpp
+++ core/js_function.hpp
@@ -3,6 +3,21 @@
 
 #include <QString>
 
-QString js_scroll_to_window_height(qreal y);
+/**
+ * @brief keep scrolling the window until
+ * Math.abs(window.pageYOffset - document height) < threshold
+ * @param threshold self explained
+ * @return JS command, for debug only
+ */
+QString js_scroll_to_window_height(qreal threshold);
+
+/**
+ * @brief keep scrolling the window until
+ * document height < cur_height || Math.abs(document height - window.pageYOffset) < threshold
+ * @param threshold self explained
+ * @return JS command, for debug only
+ * @todo give a better name
+ */
+QString js_scroll_to_window_height_2(qreal threshold);
 
 #endif // JS_FUNCTION_HPP
