d62a271064c6ab4245147d3dc57d3514e1abd226
stereomatchingkiss <thamngapwei@gmail.com>
2017-05-25 02:06:50 +0800

    move frequently use constant into global_constant class

---
+++ core/global_constant.cpp
@@ -0,0 +1,8 @@
+#include "global_constant.hpp"
+
+#include <QObject>
+
+QString global_constant::bing_search_name()
+{
+   return QObject::tr("Bing");
+}
---
+++ core/global_constant.hpp
@@ -0,0 +1,12 @@
+#ifndef GLOBAL_CONSTANT_HPP
+#define GLOBAL_CONSTANT_HPP
+
+#include <QString>
+
+class global_constant
+{
+public:
+    static QString bing_search_name();
+};
+
+#endif // GLOBAL_CONSTANT_HPP
