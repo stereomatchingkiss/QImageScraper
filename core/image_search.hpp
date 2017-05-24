b2401a99e370cc315161c468321a94249fa9c619
stereomatchingkiss <thamngapwei@gmail.com>
2017-05-24 12:37:06 +0800

    add comments

--- core/image_search.hpp
+++ core/image_search.hpp
@@ -23,7 +23,7 @@ public:
     explicit image_search(QWebEnginePage &page, QObject *parent = nullptr);
 
     /**
-     * @brief go to the first page of the search engine. Will
+     * @brief Asynchronous method, go to the first page of the search engine. Will
      * emit signal "go_to_first_page_done" after fist page is loaded.
      * Bing : https://www.bing.com/?scope=images&nr=1&FORM=NOFORM
      * Google : https://images.google.com/
@@ -58,7 +58,7 @@ public:
                                  std::function<void(QString const&, QString const&)> callback) = 0;
 
     /**
-     * @brief scroll second page of the search engine. This function
+     * @brief Asynchronous method, scroll second page of the search engine. This function
      * will emit signal "scroll_second_page_done" after scroll action
      * are all done; Will emit signal "second_page_scrolled" everytime
      * the page scroll(if oage scrolling action is trigger by scroll_second_page)
@@ -68,9 +68,21 @@ public:
 
 signals:
     void error_msg(QString const &msg);
+    /**
+     * @brief emit when go_to_first_page done
+     */
     void go_to_first_page_done();
-    void go_to_second_page_done();    
+    /**
+     * @brief emit when go_to_second_page done
+     */
+    void go_to_second_page_done();
+    /**
+     * @brief emit when scroll_second_page done
+     */
     void scroll_second_page_done();
+    /**
+     * @brief emit whenever second page scrolled
+     */
     void second_page_scrolled();
 
 protected:
