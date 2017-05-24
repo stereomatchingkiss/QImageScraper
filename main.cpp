711eea538bd09127cc86f498ea752dd5a93dffb2
stereomatchingkiss <thamngapwei@gmail.com>
2017-05-25 02:08:34 +0800

    add global settings for QSettings

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QCoreApplication::setOrganizationName("ThamSoft");
+    QCoreApplication::setOrganizationDomain("https://github.com/stereomatchingkiss");
+    QCoreApplication::setApplicationName("QImageScraper");
+
     MainWindow w;
     w.show();
 
