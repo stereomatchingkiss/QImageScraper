0799c2bf7347489af1f8ff1d963beabcaa823d52
stereomatchingkiss <thamngapwei@gmail.com>
2017-06-02 18:00:59 +0800

    increase version from 1.1 to 1.2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -52,7 +52,7 @@ MainWindow::MainWindow(QWidget *parent) :
         restoreGeometry(settings.value("geometry").toByteArray());
     }
 
-    settings.setValue("version", "1.1");
+    settings.setValue("version", "1.2");
 
     qsrand(std::time(0));
 
