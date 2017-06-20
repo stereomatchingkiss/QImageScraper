b2d37baab16b7cd019af05db2bf6b7798d6b89e4
stereomatchingkiss <thamngapwei@gmail.com>
2017-06-21 00:13:52 +0800

    first commit, have not tested

---
+++ core/tor_controller.cpp
@@ -0,0 +1,56 @@
+#include "tor_controller.hpp"
+
+#include <QsLog.h>
+#include <QByteArray>
+#include <QTcpSocket>
+#include <QTimer>
+
+tor_controller::tor_controller(QObject *parent) :
+    QObject(parent),
+    socket_(new QTcpSocket(this)),
+    state_(tor_state::authenticate)
+{
+    connect(socket_, &QTcpSocket::connected, [this]()
+    {
+        QLOG_INFO()<<"connect to tor";
+        QString const contents(QString("AUTHENTICATE \"%1\"\r\n").arg(password_));
+        //socket_->write(contents.toLatin1());
+    });
+    connect(socket_, static_cast<void(QTcpSocket::*)(QAbstractSocket::SocketError)>(&QTcpSocket::error),
+            this, &tor_controller::handle_error);
+    connect(socket_, &QTcpSocket::readyRead, this, &tor_controller::handle_ready_read);
+    connect(timer_, &QTimer::timeout, [this]()
+    {
+        emit error_happen("Tor server do not give any response");
+    });
+}
+
+void tor_controller::renew_ip(const QString &host, quint16 port, const QString &password)
+{
+    socket_->abort();
+    socket_->connectToHost(host, port);
+    timer_->start(5000);
+    password_ = password;
+}
+
+void tor_controller::handle_error(QAbstractSocket::SocketError error)
+{
+    timer_->stop();
+    QLOG_INFO()<<"connect to tor error occur:"<<error;
+    emit error_happen(socket_->errorString());
+}
+
+void tor_controller::handle_ready_read()
+{
+    timer_->stop();
+    if(socket_->bytesAvailable() >= 3){
+        if(socket_->readAll().startsWith("250")){
+            if(state_ == tor_state::authenticate){
+                emit authenticate_success();
+                socket_->write("SIGNAL NEWNYM\r\n");
+            }else{
+                emit renew_ip_success();
+            }
+        }
+    }
+}
---
+++ core/tor_controller.hpp
@@ -0,0 +1,40 @@
+#ifndef TOR_CONTROLLER_HPP
+#define TOR_CONTROLLER_HPP
+
+#include <QObject>
+#include <QAbstractSocket>
+
+class QTcpSocket;
+class QTimer;
+
+class tor_controller : public QObject
+{
+    Q_OBJECT
+public:
+    explicit tor_controller(QObject *parent = nullptr);
+
+    void renew_ip(QString const &host, quint16 port, QString const &password);
+
+signals:
+    void authenticate_success();
+    void error_happen(QString const &msg);
+    void renew_ip_success();
+
+private:
+    enum class tor_state{
+        authenticate,
+        new_ip
+    };
+
+    void handle_error(QAbstractSocket::SocketError error);
+    void handle_ready_read();
+
+    QString host_;
+    quint16 port_;
+    QString password_;
+    QTcpSocket *socket_;
+    tor_state state_;
+    QTimer *timer_;
+};
+
+#endif // TOR_CONTROLLER_HPP
