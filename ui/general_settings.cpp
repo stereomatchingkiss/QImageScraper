2dbcec1a7be714a822419c82c4242cc9eb162bfb
stereomatchingkiss <thamngapwei@gmail.com>
2017-06-22 22:27:22 +0800

    use const variable to replace hard code string, reduce replicate codes and chance of commit bugs

--- ui/general_settings.cpp
+++ ui/general_settings.cpp
@@ -9,6 +9,21 @@
 #include <QSettings>
 #include <QStandardPaths>
 
+namespace{
+
+struct setting_keys
+{
+    static QString const general_save_at;
+    static QString const general_search_by;
+    static QString const max_download_img;
+};
+
+QString const setting_keys::general_save_at("general/save_at");
+QString const setting_keys::general_search_by("general/search_by");
+QString const setting_keys::max_download_img("general/max_download_img");
+
+}
+
 general_settings::general_settings(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::general_settings),
@@ -22,27 +37,27 @@ general_settings::general_settings(QWidget *parent) :
     ui->comboBoxSearchBy->addItem(global_constant::yahoo_search_name());
 
     QSettings settings;
-    if(settings.contains("general/save_at")){
-        if(settings.value("general/save_at").toString().isEmpty()){
+    if(settings.contains(setting_keys::general_save_at)){
+        if(settings.value(setting_keys::general_save_at).toString().isEmpty()){
             ui->lineEditSaveAt->setText(write_able_path_);
         }else{
-            ui->lineEditSaveAt->setText(settings.value("general/save_at").toString());
+            ui->lineEditSaveAt->setText(settings.value(setting_keys::general_save_at).toString());
         }
     }else{
         ui->lineEditSaveAt->setText(write_able_path_);
-        settings.setValue("general/save_at", ui->lineEditSaveAt->text());
+        settings.setValue(setting_keys::general_save_at, ui->lineEditSaveAt->text());
     }
 
-    if(settings.contains("general/search_by")){
-        ui->comboBoxSearchBy->setCurrentText(settings.value("general/search_by").toString());
+    if(settings.contains(setting_keys::general_search_by)){
+        ui->comboBoxSearchBy->setCurrentText(settings.value(setting_keys::general_search_by).toString());
     }else{
-        settings.setValue("general/search_by", ui->comboBoxSearchBy->currentText());
+        settings.setValue(setting_keys::general_search_by, ui->comboBoxSearchBy->currentText());
     }
 
-    if(settings.contains("general/max_download_img")){
-        ui->spinBoxMaxDownloadImg->setValue(settings.value("general/max_download_img").toInt());
+    if(settings.contains(setting_keys::max_download_img)){
+        ui->spinBoxMaxDownloadImg->setValue(settings.value(setting_keys::max_download_img).toInt());
     }else{
-        settings.setValue("general/max_download_img", ui->spinBoxMaxDownloadImg->value());
+        settings.setValue(setting_keys::max_download_img, ui->spinBoxMaxDownloadImg->value());
     }
 }
 
@@ -54,14 +69,14 @@ general_settings::~general_settings()
 void general_settings::accept_settings()
 {
     QSettings settings;
-    if(ui->comboBoxSearchBy->currentText() != settings.value("general/search_by").toString()){
+    if(ui->comboBoxSearchBy->currentText() != settings.value(setting_keys::general_search_by).toString()){
         search_by_changed_ = true;
     }else{
         search_by_changed_ = false;
     }
 
-    settings.setValue("general/search_by", ui->comboBoxSearchBy->currentText());
-    settings.setValue("general/max_download_img", ui->spinBoxMaxDownloadImg->value());
+    settings.setValue(setting_keys::general_search_by, ui->comboBoxSearchBy->currentText());
+    settings.setValue(setting_keys::max_download_img, ui->spinBoxMaxDownloadImg->value());
 
     QDir dir;
     if(!dir.mkpath(ui->lineEditSaveAt->text())){
@@ -72,7 +87,7 @@ void general_settings::accept_settings()
         ui->lineEditSaveAt->setText(write_able_path_);
     }
 
-    settings.setValue("general/save_at", ui->lineEditSaveAt->text());
+    settings.setValue(setting_keys::general_save_at, ui->lineEditSaveAt->text());
 }
 
 int general_settings::get_max_download_img() const
@@ -103,9 +118,9 @@ void general_settings::reject_settings()
 void general_settings::restore_value()
 {
     QSettings settings;
-    ui->lineEditSaveAt->setText(settings.value("general/save_at").toString());
-    ui->comboBoxSearchBy->setCurrentText(settings.value("general/search_by").toString());
-    ui->spinBoxMaxDownloadImg->setValue(settings.value("general/max_download_img").toInt());
+    ui->lineEditSaveAt->setText(settings.value(setting_keys::general_save_at).toString());
+    ui->comboBoxSearchBy->setCurrentText(settings.value(setting_keys::general_search_by).toString());
+    ui->spinBoxMaxDownloadImg->setValue(settings.value(setting_keys::max_download_img).toInt());
 }
 
 void general_settings::closeEvent(QCloseEvent *event)
@@ -119,7 +134,7 @@ void general_settings::closeEvent(QCloseEvent *event)
 void general_settings::on_pushButtonSaveAt_clicked()
 {
     QString const dir = QFileDialog::getExistingDirectory(this, tr("Open/Create Save At directory"),
-                                                          QSettings().value("general/save_at").toString(),
+                                                          QSettings().value(setting_keys::general_save_at).toString(),
                                                           QFileDialog::ShowDirsOnly
                                                           | QFileDialog::DontResolveSymlinks);
     ui->lineEditSaveAt->setText(dir);
